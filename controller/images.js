74da14a1f0239d72477a89dfef94c03d6bfdf6a5
TesslaRay <cristian.valdivia@alumnos.usm.cl>
2019-11-04 00:15:57 -0300

    Upload and predict

--- controller/images.js
+++ controller/images.js
@@ -1,15 +1,36 @@
 const multer = require('multer');
-const {sendUploadToGCS, getPublicUrl} = require('../lib/images');
+const fs = require('fs');
 
-const storage = multer({
+const {sendUploadToGCS} = require('../lib/images');
+const {predictWithImage} = require('../services/googleAutoML');
+
+const temporalStorage = multer({
+
   storage: multer.MemoryStorage,
   limits: {
     fileSize: 25 * 1024 * 1024, // no larger than 25mb
   },
 });
 
-exports.chargeImage = async (req, res, next) => {
-    const upload = storage.single('file');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './resources')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  }
+});
+
+exports.chargeImage = async (req, res) => {
+    const upload = temporalStorage.single('file');
+
+    const saveImage = multer({ storage: storage }).single('file')
+
+    await saveImage(req, res, function (err) {  
+      predictWithImage(req.file.path);
+      fs.unlinkSync(req.file.path); 
+    });
+    
     await upload(req, res, async function (err) { 
       await sendUploadToGCS(req, res);
     });
